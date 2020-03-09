6147cfcca656bacbffe3a48dd308f0fb0374dc73
majkel <michal.sobotka.84@gmail.com>
2020-03-09 23:02:11 +0100

    class linked with main

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main()
 {
-    BattleShip bs;
-    bs.createEmptyBoard(5);
-    bs.setShips(5);
-    bs.fire(1,2);
+    //BattleShip bs;
+    //bs.createEmptyBoard(5);
+    //bs.setShips(5);
+    //bs.fire(1,2);
 
     return 0;
 }
