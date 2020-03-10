e2902a220bb5fc1c4dda580acd5b7a402dc76880
majkel <michal.sobotka.84@gmail.com>
2020-03-10 12:46:49 +0100

    Destrucotr with releasing alocated memory for game board

--- src/BattleShip.cpp
+++ src/BattleShip.cpp
@@ -52,9 +52,9 @@ int BattleShip::getShipsLeftOnBoard()
 
 BattleShip::~BattleShip()
 {
-    /*for(int i = 0; i < rows_lines_; ++i)
+    for(int i = 0; i < rows_lines_; ++i)
     {
         delete [] board[i];
     }
-    delete [] board;*/
+    delete [] board;
 }
