7e2daf9d7d6c0929b93fd64afc73d76947118700
majkel <michal.sobotka.84@gmail.com>
2020-03-11 22:22:27 +0100

    first test add

--- test/TestBattleShip.cpp
+++ test/TestBattleShip.cpp
@@ -3,7 +3,13 @@
 
 struct TestArrays : public ::testing::Test
 {
-
+    BattleShip bs;
 };
+
+TEST_F(TestArrays, checkRotationByThree)
+{
+    bs.setShips(5);
+    ASSERT_FALSE(bs.fire(0,0));
+}
 
 
