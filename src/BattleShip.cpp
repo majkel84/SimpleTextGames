be91cc3604ae7596e069c47d56f9e2ad74b4b25b
majkel <michal.sobotka.84@gmail.com>
2020-03-12 22:29:24 +0100

    second function to create game board

--- src/BattleShip.cpp
+++ src/BattleShip.cpp
@@ -3,7 +3,15 @@
 BattleShip::BattleShip()
 {
     for(int i = 0; i < rows_lines_; ++i)
-        board[i] = new int[rows_lines_];
+        board_[i] = new int[rows_lines_];
+}
+
+int** BattleShip::createBoard(int boardSize)
+{
+    int** board = new int*[boardSize];
+    for(int i = 0; i < boardSize; ++i)
+        board[i] = new int[boardSize];
+    return board;
 }
 
 void BattleShip::showBoard()
@@ -13,9 +21,16 @@ void BattleShip::showBoard()
         {
             cout << endl;
             for (int lines = 0; lines < rows_lines_; lines++)
-                cout << board[rows][lines] << " ";
+                cout << board_[rows][lines] << " ";
         }
 }
+
+/*void BattleShip::showBoard2(int **b)
+{
+    for(int i = 0; i < sizeof(b)/sizeof(b[0]); i++)
+        for (int j = 0; j <  sizeof(b)/sizeof(b[0]); j++)
+            cout << b[i][j] << " ";
+}*/
 
 void BattleShip::setShips(unsigned short numberOfShips)
 {
@@ -25,10 +40,10 @@ void BattleShip::setShips(unsigned short numberOfShips)
     {
         int x = rand() % (rows_lines_ - 1);
         int y = rand() % (rows_lines_ - 1);
-        if (board[x][y] != 1)
+        if (board_[x][y] != 1)
         {
             ships++;
-            board[x][y] = 1;
+            board_[x][y] = 1;
         }
     }
 }
@@ -37,9 +52,9 @@ bool BattleShip::fire(int x, int y)
 {
     if (x >= rows_lines_ || y >= rows_lines_)
         return false;
-    if (board[x][y] == 1)
+    if (board_[x][y] == 1)
     {
-        board[x][y] = 2;
+        board_[x][y] = 2;
         shipsLeftOnBoard--;
         cout << "Got It" << endl;
         showBoard();
@@ -57,6 +72,6 @@ int BattleShip::getShipsLeftOnBoard()
 BattleShip::~BattleShip()
 {
     for(int i = 0; i < rows_lines_; i++)
-        delete [] board[i];
-    delete [] board;
+        delete [] board_[i];
+    delete [] board_;
 }
--- src/BattleShip.hpp
+++ src/BattleShip.hpp
@@ -9,7 +9,9 @@ class BattleShip
 {
 public:
     BattleShip();
+    int** createBoard(int boardSize);
     void showBoard();
+    //void showBoard2(int** b);
     void setShips(unsigned short numberOfShips);
     bool fire(int x, int y);
     int getShipsLeftOnBoard();
@@ -17,6 +19,6 @@ public:
 
 private:
     unsigned short rows_lines_ = 5;
-    int** board = new int*[rows_lines_];
+    int** board_ = new int*[rows_lines_];
     unsigned short shipsLeftOnBoard;
 };
