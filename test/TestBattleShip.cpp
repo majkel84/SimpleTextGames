5d5e8c2bda5c24f42f8e28ed2035514929ab6bcb
majkel <michal.sobotka.84@gmail.com>
2020-03-12 22:07:14 +0100

    test for better codecov %

--- test/TestBattleShip.cpp
+++ test/TestBattleShip.cpp
@@ -6,10 +6,16 @@ struct TestBattleShip : public ::testing::Test
     BattleShip bs;
 };
 
-TEST_F(TestBattleShip, checkWronFire)
+TEST_F(TestBattleShip, checkWrongShot)
 {
     bs.setShips(5);
     ASSERT_FALSE(bs.fire(0,0));
+}
+
+TEST_F(TestBattleShip, checkShipsLeft)
+{
+    bs.setShips(5);
+    ASSERT_EQ(bs.getShipsLeftOnBoard(),5);
 }
 
 
