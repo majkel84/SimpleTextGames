8ca44fda2092f0b665eaeb05226359a47560b944
majkel <michal.sobotka.84@gmail.com>
2020-03-23 22:23:31 +0100

    first test for OX game

--- test/TestOX.cpp
+++ test/TestOX.cpp
@@ -7,7 +7,10 @@ struct TestOX : public ::testing::Test
     OX ox;
 };
 
-/*TEST_F(TestOX, checkSetField)
+TEST_F(TestOX, checkSetField)
 {
-
-}*/
+    ox.chooseField(1,1);
+    ox.chooseField(1,1);
+    char testValue = ox.getBoardField(1,1);
+    EXPECT_EQ((char)testValue, 88);
+}
