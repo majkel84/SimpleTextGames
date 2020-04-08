36589ea406b13276c58b8254f7ae1922ca0c7116
majkel <michal.sobotka.84@gmail.com>
2020-04-08 13:51:49 +0200

    methodes to start snake, and randomly put fruit on board

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -2,9 +2,26 @@
 
 void Snake::setup()
 {
-   Snake::input();
-   Snake::show();
-   Snake::logic();
+    Snake::setStartPosition();
+    Snake::putFruitOnBoard();
+    while(!gameOver)
+    {
+        Snake::input();
+        Snake::show();
+        Snake::logic();
+    }
+}
+
+void Snake::setStartPosition()
+{
+    snakeX = width / 2;
+    snakeY = height / 2;
+}
+
+void Snake::putFruitOnBoard()
+{
+    fruitX = rand() % width;
+    fruitY = rand() % height;
 }
 
 void Snake::input()
--- src/Snake.hpp
+++ src/Snake.hpp
@@ -7,11 +7,19 @@ using namespace std;
 
 class Snake
 {
-    void logic();
     void setup();
+    void setStartPosition();
+    void putFruitOnBoard();
     void input();
     void show();
+    void logic();
 
 private:
     bool gameOver = false;
+    const unsigned short width = 20;
+    const unsigned short height = 20;
+    unsigned short snakeX, snakeY;
+    unsigned short fruitX,fruitY;
+    enum enumDirection {STOP, LEFT, RIGHT, UP, DOWN};
+    enumDirection direction = STOP;
 };
